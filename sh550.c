e7ad1dbbc150a008bcc205ac08035b830ed28d8a
Sweekrithi  S  Shetty <shettysweekrithi03@gmail.com>
2021-04-05 18:46:20 -0400

    Add files via upload

--- sh550.c
+++ sh550.c
@@ -5,6 +5,7 @@
 #include <assert.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <fcntl.h> 
 
 //limits
 #define MAX_TOKENS 100
@@ -35,13 +36,13 @@ proc_job *back_jobs;
 void initialize()
 {
     //Allocate space for the whole line
-	assert( (line = malloc(sizeof(char) * MAX_STRING_LEN)) != NULL);
+	assert( (line = malloc(sizeof(char) *MAX_STRING_LEN)) != NULL);
 	//Allocate space for individual tokens
 	assert( (tokens = malloc(sizeof(char*)*MAX_TOKENS)) != NULL);
 	//Open stdin as a file pointer 
 	assert( (fp = fdopen(STDIN_FILENO, "r")) != NULL);
 	//Allocate space for background jobs
-	assert((back_jobs = malloc(sizeof( proc_job) * MAX_TOKENS)) != NULL);
+	assert((back_jobs = malloc(sizeof( proc_job) *MAX_TOKENS)) != NULL);
 }
 
 int tokenize (char *string)
@@ -58,7 +59,7 @@ int tokenize (char *string)
 		} 
 		//Storing the number of counts intiated
         tokens[token_count] = this_token;
-        printf("Token %d: %s\n", token_count, tokens[token_count]);
+        //printf("Token %d: %s\n", token_count, tokens[token_count]);
 		token_count++;
 		// if there are more tokens than space ,reallocate more space
 		if(token_count >= size){
@@ -79,20 +80,21 @@ int read_command()
 void  listjobs()
 {
 	int counter = 0; 
-	for (int i = 0; i<MAX_TOKENS; ++i)
+	int i = 0;
+	while(i<MAX_TOKENS)
 	{
 		pid_t id = back_jobs[i].pid;
-			int status, child_1;
-			//returns the status of the child process
-			child_1 = waitpid(id, &status, WNOHANG);
+		int status, child_1;
+		//returns the status of the child process
+		child_1 = waitpid(id, &status, WNOHANG);
 		if(back_jobs[i].name)
 		{
 			counter++;
-			if (child_1 > 0)
+			if(child_1>0)
 			{
 				printf("Command %s with PID %d Status: %s \n", back_jobs[i].name, back_jobs[i].pid, "Finished");	
 			}
-			else if (child_1 <0)
+			else if(child_1<0)
 			{
 				printf("Command %s with PID %d Status: %s \n", back_jobs[i].name, back_jobs[i].pid, "Finished");	
 			}
@@ -101,6 +103,7 @@ void  listjobs()
 				printf("Command %s with PID %d Status: %s \n", back_jobs[i].name, back_jobs[i].pid, "Running");
 			}
 		}
+		i++;
 	}
 
 }
@@ -110,34 +113,249 @@ void bg_to_fg(int pid){
 		
 }
 
+void pipe_execute( int total_tokens, int delimit){
+	int fds[2];
+	char ** c1 = malloc(sizeof(char*)*delimit);
+	char ** file2 = malloc(sizeof(char*)*(total_tokens-delimit -1));
+	int status;
+	int child; 
+	pid_t pid1, pid2;
+	
+	if(pipe(fds) == -1){
+		perror("Pipe error");
+	}
+	
+	int i = 0;
+	while(i<(total_tokens-delimit-1))
+	{
+		file2[i] = tokens[i+delimit+1];
+		printf("%s\n", file2[i]);
+		i++;
+	}
+
+	int j=0;
+	while(j<delimit)
+	{
+		c1[j]= tokens[j];
+		j++;
+	}
+	
+	pid1 = fork();
+	if(pid1 == 0)
+	{
+		close(1);
+		if(dup2(fds[1], 1)<0){
+			fprintf(stderr, "Error \n");
+		}
+		close(fds[0]);
+		execvp(c1[0], c1);
+	}
+	else if(pid1 < 0)
+	{
+		exit(1);
+	}
+	
+	pid2 = fork();
+	if(pid2==0)
+	{
+		close(0);
+		dup2(fds[0],0);
+		close(fds[1]);
+		execvp(file2[0], file2);
+	}
+	else if(pid2< 0)
+	{
+		exit(1);
+	}
+	close(fds[0]);
+	close(fds[1]);
+
+
+	child = waitpid(pid1,&status,0);
+	child = waitpid(pid2,&status,0);	
+	if (child > 0)
+	{
+		free(tokens);
+		assert((tokens = malloc(sizeof(char*)*MAX_TOKENS)) != NULL);
+	}
+	free(c1);
+	free(file2);
+}
+
+void input_redirection(int delimit){
+	int fds[2];
+	char * file1 = malloc(sizeof(char*));
+	char ** c1 = malloc(sizeof(char*)*delimit);
+	pid_t pid1, pid2;
+	file1=tokens[delimit+1];
+	int status;
+	int child; 
+	
+	int i=0;
+	while(i<delimit)
+	{
+		c1[i]=tokens[i];
+		i++;
+	}
+	
+	pid1=fork();
+	if(pid1==0)
+	{
+		close(1);
+		if(dup2(fds[1],1)<0)
+		{
+			fprintf(stderr, "Error");
+		}
+		close(fds[0]);
+		FILE * file=fopen(file1, "r");
+
+		while(file==NULL)
+		{
+			exit(1);
+		}
+		char s1 = fgetc(file);
+		while(s1!= EOF)
+		{
+			putchar(s1);
+			s1=fgetc(file);		
+		}
+		fclose(file);
+		exit(0);
+	}
+	else if(pid1<0)
+	{
+		exit(1);
+	}
+	
+	pid2=fork();	
+	if(pid2==0)
+	{
+		close(0); 
+		dup2(fds[0],0); 
+		close(fds[1]); 
+		execvp(c1[0], c1);
+	}
+	else if(pid2<0){
+		exit(1);
+	}
+	
+	close(fds[0]);
+	close(fds[1]);
+
+	child=waitpid(pid1,&status,0);
+	child=waitpid(pid2,&status,0);	
+	if (child>0)
+	{
+		free(tokens);
+		assert((tokens= malloc(sizeof(char*)*MAX_TOKENS))!= NULL);
+	}
+}
+void output_redirection(int delimit){
+	pid_t pid;
+	char ** file2 = malloc(sizeof(char*));
+	char ** c1 = malloc(sizeof(char*)*delimit);
+	
+	int i=0;
+	while(i<delimit)
+	{
+		c1[i]= tokens[i];
+		i++;
+	}
+
+	file2[0] = tokens[delimit+1];
+	int file = open(file2[0] , O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRGRP| S_IROTH);	 
+	
+	pid=fork();
+	if(pid>0)
+	{	
+		close(file);
+		int status;
+		int child; 
+		child = waitpid(pid,&status,0);	
+		if (child>0)
+		{
+			free(tokens);
+			assert((tokens = malloc(sizeof(char*)*MAX_TOKENS))!= NULL);
+		}
+		free(c1);
+		free(file2);
+	}
+	
+	else if(pid==0)
+	{
+		close(1);
+		if(dup2(file, 1) <0)
+		{
+			fprintf(stderr, "Error \n");
+		}
+		c1[delimit] = '\0';	
+		execvp(c1[0],c1);
+		exit(0);
+	}
+	else
+	{
+		exit(1);
+	}
+}
+
+int test_rtype(int total_tokens){
+	int i = 0;
+	int r = 0;
+	while(i<total_tokens && !r)
+	{
+		if(strcmp(tokens[i],"|") == 0)
+		{
+			pipe_execute(total_tokens, i);
+			r= 1;
+		}
+		else if(strcmp(tokens[i],"<") == 0)
+		{
+			input_redirection(i);
+			r= 1;
+		}
+		else if(strcmp(tokens[i],">") == 0)
+		{
+			output_redirection(i);
+			r= 1;
+		}
+		i++;		
+	}
+	return r;
+}
 
 void execute(int total_tokens)
 {
-	pid_t pid; 
-    //creating  a new process by duplicating the calling process
-	pid = fork();
-    //Condition
-    if (pid < 0){
-		perror("Fork failed:");
-		exit(1);
-	}
-	else if(pid ==0){	
-		if(execvp(tokens[0],tokens) == -1)
-		{
-			fprintf(stderr, "Command not found \n");
+	int mode = test_rtype(total_tokens);
+	if (!mode)
+	{
+		pid_t pid; 
+		//creating  a new process by duplicating the calling process
+		pid = fork();
+		//Condition
+		if (pid<0){
+			exit(1);
 		}
-		exit(0);
-	}
-	else{
-		int child_2; 
-		int status;
-		child_2 = waitpid(pid,&status,0);	
-		if (child_2 > 0){
-			free(tokens);
-			assert( (tokens = malloc(sizeof(char*)*MAX_TOKENS)) != NULL);
+		else if(pid==0){
+			tokens[total_tokens] = '\0';
+			if(execvp(tokens[0],tokens) == -1)
+			{
+				fprintf(stderr, "Invalid command\n");
+			}
+			exit(0);
 		}
-	}	
+		else{
+			int child; 
+			int status;
+			child= waitpid(pid,&status,0);
+			if (child>0){
+				free(tokens);
+				assert((tokens = malloc(sizeof(char*)*MAX_TOKENS)) != NULL);
+			}
+		}	
+	}
+	
 }
+
 int run_command(int total_tokens) {
 	//Verifying if token is empty
 	while(!tokens[0]){
@@ -181,7 +399,6 @@ int run_command(int total_tokens) {
 				pid = fork();
 				if (pid < 0)
 				{
-					perror("Fork failed:");
 					exit(1);
 				}
 				else if(pid ==0)
@@ -196,7 +413,7 @@ int run_command(int total_tokens) {
 				else
 				{ 
 					char *s1; 
-					s1= malloc(sizeof(char *) * MAX_TOKENS);
+					s1= malloc(sizeof(char *)* MAX_TOKENS);
 					proc_job  jobs  = {s1, pid, 0};
 					strcpy(s1, tokens[0]);
 					int i = 0; 
